b532955a8df6013e7e127f9b22008b148fa84856
Askuri <enwi2@t-online.de>
2018-07-13 10:21:01 +0200

    Cleanup of templates Enhanced security check for upload listener

--- src/EventListener/UploadValidationListener.php
+++ src/EventListener/UploadValidationListener.php
@@ -11,6 +11,8 @@ use App\Service\Security;
 use Oneup\UploaderBundle\Event\ValidationEvent;
 use Oneup\UploaderBundle\Uploader\Exception\ValidationException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 
 class UploadValidationListener
@@ -24,17 +26,15 @@ class UploadValidationListener
         $this->security = $security;
     }
     
-    public function onValidate(ValidationEvent $event)
+    public function onValidate(ValidationEvent $event, AuthorizationCheckerInterface $authChecker)
     {
-        $login = $this->tokenStorage->getToken()->getUser()->getUsername();
-        /*
-        $config  = $event->getConfig();
-        $file    = $event->getFile();
-        $type    = $event->getType();
-         */
-        $request = $event->getRequest();
+        if (!$authChecker->isGranted('ROLE_USER')) {
+            throw new AccessDeniedException('Only logged in users allowed to upload!');
+        }
         
-        $raw_path  = $request->get('path');
+        $login = $this->tokenStorage->getToken()->getUser()->getUsername();
+        $request = $event->getRequest();
+        $raw_path = $request->get('path');
         
         $this->checkPath($raw_path, $login);
     }
@@ -55,37 +55,10 @@ class UploadValidationListener
         if (false === $this->security->checkDirUp($path, false)) {
             throw new ValidationException('Usage of ../ not allowed! Please report this bug or stop cracking.');
         }
-            
-        /*
-        if (strpos($path, '../')) {
-            if ($throw_errors) {
-                throw new ValidationException('Usage of ../ not allowed! Please report this bug or stop cracking.');
-            }
-            return false;
-        }
-         * 
-         */
         
         //step 2)
         if ($login != $this->security->pathLogin($path)) {
             throw new ValidationException('Uploading outside user\'s directory is not allowed.');
         }
-        
-        /* REMOVE ME
-        $path = trim($path, '/');
-        
-        $array = explode('/', $path);
-        $dir_user = $array[0];
-        if ($dir_user === $login) {
-            unset($array[0]);
-            return implode('/', $array);
-        } else {
-            if ($throw_errors) {
-                throw new ValidationException('Uploading outside user\'s directory is not allowed: '.$dir_user);
-            }
-            return false;
-        }
-         * 
-         */
     }
 }
