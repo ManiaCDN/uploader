a11b374c0161bf16561af1447c9817862ec8fb62
Martin Weber <enwi2@t-online.de>
2022-01-13 19:20:03 +0100

    Use chunked uploads

--- public/js/dropzone.config.js
+++ public/js/dropzone.config.js
@@ -1,16 +1,14 @@
-// Thanks to https://j11y.io/javascript/bujs-1-getparameterbyname/
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-}
-
 // use the camelized version of HTML element id
 Dropzone.options.uploadBrowse = {
     // The name that will be used to transfer the file
     paramName: "file",
     
     maxFilesize: 512, // MB
-    
+
+    chunking: true,
+    forceChunking: true,
+    chunkSize: 1000000,
+
     // Timeout after which the upload should be aborted. Default is 30000.
     // Given in microseconds. Does not trigger an error on timeout...
     timeout: 86400000,
@@ -25,12 +23,6 @@ Dropzone.options.uploadBrowse = {
     
     // don't show it because it might be confusing while not being so useful
     dictRemoveFile: "",
-    
-    params: {
-        // pass the GET path variable to the serverside upload function,
-        // so it knows where to store the file.
-        path: getParameterByName('path'),
-    },
     
     init: function() {
         this.on("success", function(file) {
