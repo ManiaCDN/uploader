2df4e21e9cbfa843c70f3446c0623112d5593002
Martin Weber <enwi2@t-online.de>
2023-04-29 14:28:42 +0200

    feat: add testHomeWithText

--- tests/Application/HomeTest.php
+++ tests/Application/HomeTest.php
@@ -2,16 +2,42 @@
 
 namespace App\Tests\Application;
 
+use App\Entity\Setting;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeTest extends WebTestCase {
 
-    public function testHomeDefaultText(): void {
-        $client = static::createClient();
+    private KernelBrowser $client;
+    private EntityManagerInterface $entityManager;
 
-        $crawler = $client->request('GET', '/');
+    public function setUp(): void {
+        $this->client = static::createClient();
+        $this->entityManager = $this->getContainer()->get(EntityManagerInterface::class);
+
+        parent::setUp();
+    }
+
+    public function testHomeDefaultText(): void {
+        $this->client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
-        $this->assertSelectorTextContains('h1', 'No welcome set yet');
+        $this->assertSelectorTextContains('h1', 'No welcome set yet!');
+    }
+
+    public function testHomeWithText(): void {
+        $testMessage = "# test message";
+
+        $setting = new Setting();
+        $setting->setName('welcome_message');
+        $setting->setValue('# ' . $testMessage);
+        $this->entityManager->persist($setting);
+        $this->entityManager->flush();
+
+        $this->client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h1', $testMessage);
     }
 }
